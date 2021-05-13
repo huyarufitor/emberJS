cb2c328d1a5812b89b81fc81e2d863cb23f29c5f
fitorhu <fitorhu@zego.im>
2021-05-13 10:19:03 +0800

    删除fitor-vue 分支代码，新建ts+vue

--- app/adapters/application.js
+++
@@ -1,8 +0,0 @@
-import WildemberAdapter from 'wildember/adapters/wildember';
-
-export default WildemberAdapter.extend({
-    wilddogConfig: {
-        syncDomain: "wildember.wilddog.com",
-        syncURL: "https://wildember.wilddogio.com" //输入节点 URL
-    }
-});
